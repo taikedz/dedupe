b57fb4263f4c30867c5c57ade7b7a884b02df938
Tai Kedzierski <dch.tai@gmail.com>
2020-04-19 04:21:19 +0100

    model base file layout

---
+++ install.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+PYTHONBIN=python3 # the Python 3.x command
+LIBPATH=/usr/local/lib # where the dedupe python files are kept
+
+# TODO
+
+# 1 copy files to destinations
+# 2 sed s/// the placeholders at destination
