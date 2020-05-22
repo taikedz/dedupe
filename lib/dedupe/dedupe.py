1eb131b6cc5b98124c2eb0d4f3d27dff57482a20
Tai Kedzierski <dch.tai@gmail.com>
2020-05-22 20:37:50 +0100

    slightly flesh out the logic in the 'main' script

--- lib/dedupe/dedupe.py
+++ lib/dedupe/dedupe.py
@@ -1,14 +1,44 @@
 #!/usr/bin/env python
 
-improt ddpreferences as DDPREF
+import ddpreferences as DDPREF
+import ddargparse
+import ddpaths
 
+def parseArguments(args):
+    return ddargparse.parseArguments(args)
+
+def loadPreferences(config_path):
+    if config_path == None:
+        config_paths = ddpaths.resolveAll(ddpaths.global_config_paths)
+        for path in config_paths:
+            if os.path.exists(path):
+                config_path = path
+                break
+
+    DDPREF.loadPreferences(config_path) # If still None, nothing will load, we are safe
+
+def loadDatabase(dbname):
+    pass
+
+def loadHandlers():
+    encounter_handler_spec = DDPREF.getPreference("handlers/encounters")
+
+    for encounter_event_name in encounter_handler_spec.keys():
+        for handler_name in encounter_handler_spec[encounter_event_name]:
+            Handlers.registerEncounterEvent(handler_name)
+
+def processAll(folders, walk_only=False, resolve_only=False):
+    if not resolve_only:
+        for top_dir in args.obj.folders:
+            walk(top_dir, args_obj.walk_only)
+
+    if not walk_only:
+        resolve(args_obj.resolve)
+    
 def main(args):
     args_obj = parseArguments(args)
-    loadPreferences(args_obj.conf)
-    loadDatabase(DDPREF.getPreference("config/engine/database"))
-    loadHandlers(DDPREF.getPreference("handlers"))
+    loadPreferences(args_obj.config)
+    loadDatabase(args_obj.database)
+    loadHandlers()
     
-    for top_dir in args.obj.folders:
-        walk(top_dir)
-
-    resolve()
+    processAll(args_obj.folders, walk_only=args_obj.walk_only, resolve_only=args_obj.resolve)
