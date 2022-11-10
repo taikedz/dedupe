b6039bdf1d7491f848077c6fa3077bb5c9573bed
Tai Kedzierski <dch.tai@gmail.com>
2022-11-10 17:28:26 +0000

    WIP stub improvements

--- dedupe/db/api_generic.py
+++ dedupe/db/api_generic.py
@@ -1,7 +1,69 @@
+
+from typing import Tuple
+from dataclasses import dataclass
+
+
+class DedupeDatabaseError(Exception): pass
+
+@dataclass
+class FieldDef:
+    name: str
+    dtype: str
+    index: bool = False
+    primary: bool = False
+    foreign_key: Tuple[str,str] = None, # foreign table, foreign name
+
+class FieldDefError(Exception): pass
+
 class DbApiGeneric:
-    def __init__(self, *args, **kwargs):
+    """ Generic Database API class.
+
+    Subclass this to implement a target database library/engine (see api_sqlite.py for an example)
+
+    This class provides genericised table definitions, so that implementing classes
+    can perform a suitable data type conversion. For example "text" can be a SQLite "TEXT"
+    type, whereas in MySQL it would be represented by a "BLOB" type perhaps.
+    """
+
+    PATHS_TABLE = [
+        FieldDef("path", "chars:256", True, True),
+        FieldDef("size", "int"),
+        FieldDef("short_hash", "chars:64"),
+        FieldDef("full_hash", "chars:64"),
+    ]
+
+    DUPLICATES_TABLE = [
+        FieldDef("full_hash", "chars:64", True, True, ("Paths", "full_hash"))
+    ]
+
+
+    def __init__(self):
+        """ Override this method.
+
+        Open a connection to your database first, then call this super-class constructor
+
+        e.g.
+
+        ```python
+        def __init__(self, **db_details):
+            # Do this first
+            dbengine.connect(**dbdetails)
+
+            DbApiGeneric.__init__(self)
+        ```
+        """
         if type(self) == DbApiGeneric:
             raise NotImplementedError("Cannot call init of DbApiGeneric. Please instantiate from a subclass.")
+
+        self.create_table("Paths", self.PATHS_TABLE)
+        self.create_table("Duplicates", self.DUPLICATES_TABLE)
+
+
+    # ==============================
+    # 'Abstract' placeholder methods
+
+    def create_table(self, table_name:str, table_def:FieldDef):
+        raise NotImplementedError()
 
 
     def add_path(self, path, size, short_hash, full_hash):
@@ -13,6 +75,10 @@ class DbApiGeneric:
 
 
     def update_path(self, path, size, short_hash, full_hash):
+        raise NotImplementedError()
+
+
+    def get_path_info(self, path):
         raise NotImplementedError()
 
 
--- dedupe/db/api_sqlite.py
+++ dedupe/db/api_sqlite.py
@@ -1,21 +1,77 @@
 import sqlite3
+from typing import List
 
-from dedupe.db.api_generic import DbApiGeneric
+from dedupe.db.api_generic import DbApiGeneric, DedupeDatabaseError, FieldDef, FieldDefError
+
+TYPES = {
+    # Workaround: Implementing 'int' type as TEXT to preserve ginormous numbers
+    "int": "TEXT",
+    "chars": "TEXT"
+}
+
+def get_field_type(type_spec:str):
+    """ Convert unified notation/type into engine's type
+    """
+    parts = type_spec.split(":")
+
+    if len(parts) <= 0:
+        raise FieldDefError(f"Empty definition {type_spec}")
+
+    else:
+        return TYPES[parts[0]]
+
 
 class SQLiteApi(DbApiGeneric):
     
     def __init__(self, db_path):
         self.db = sqlite3.connect(db_path)
 
+        DbApiGeneric.__init__(self)
+
 
     def __destroy__(self):
         self.db.close()
 
 
-    def _query(self, query, values):
-        with self.db.cursor() as c:
-            res = c.execute(query, values)
+    def create_table(self, table_name:str, table_def:List[FieldDef]):
+        field_queries = []
+
+        # First build fields-proper
+        for field in table_def:
+            subq = []
+            subq.append(f"{field.name} {get_field_type(field.dtype)}")
+
+            if field.index:
+                subq.append("INDEX")
+            if field.primary:
+                subq.append("PRIMARY KEY")
+
+            field_queries.append(" ".join(subq))
+
+        # Add foreign key definitions at the end
+        for field in table_def:
+            if field.foreign_key:
+                field_queries.append(f"FOREIGN KEY ({field.name} REFERENCES {field.foreign_key[0]} ({field.foreign_key[1]}))")
+
+        query = f"""
+        CREATE TABLE {table_name} (
+            {",\n".join(field_queries)}
+        )
+        """
+
+        self._query(query)
+
+
+    def _query(self, query, *values):
+        c = None
+        try:
+            c = self.db.cursor()
+            res = c.execute(query, *values)
             self.db.commit()
+        except:
+            if c != None:
+                c.close()
+            raise
         return res
 
 
@@ -29,14 +85,29 @@ class SQLiteApi(DbApiGeneric):
 
 
     def update_path(self, path, size=None, short_hash=None, full_hash=None):
+        info = self.get_path_info(path)
+
+        if size is None: size = info["size"]
+        if short_hash is None: short_hash = info["short_hash"]
+        if full_hash is None: full_hash = info["full_hash"]
 
         self._query("UPDATE Entries SET(size=?,short_hash=?,full_hash=?)",
-            (size,short_hash,full_hash))
+            (str(size),short_hash,full_hash))
 
 
     def get_path_info(self, path):
-        res = self._query("SELECT size,short_hash,full_hash FROM Entries WHERE path=?", (path,))
-        return res # FIXME - we probably need to unpack the data a little
+        properties = ["size","short_hash","full_hash"]
+        res = self._query(f"SELECT {','.join(properties)} FROM Entries WHERE path=?", (path,))
+        entries = [r for r in res]
+
+        if len(entries) != 1:
+            # Not likely to be more than 1, could be zero.
+            raise DedupeDatabaseError(f"Entry not found for {path}: {entries}")
+
+        info = {k:v for k,v in zip(properties, entries[0])}
+        info["size"] = int(info["size"])
+
+        return info
 
 
     def find_path_duplicates(self, path):
