2dbb9abc2992bce5e409535235ecbcbda8cf7572
Tai Kedzierski <dch.tai@gmail.com>
2022-11-26 23:15:24 +0000

    Process top level directory

--- dedupe/dedupe-main.py
+++ dedupe/dedupe-main.py
@@ -1,4 +1,3 @@
-from dedupe.db.api_sqlite import SQLiteApi
 from dedupe import ddargs
 from dedupe import event
 from dedupe.walker import DedupeWalker
@@ -9,8 +8,7 @@ def main():
 
     event.load_handlers()
 
-    db = SQLiteApi(args.database)
-    walker = DedupeWalker(db)
+    walker = DedupeWalker()
 
     for dirpath in args.folders:
         walker.walk_folder(dirpath)
--- dedupe/walker.py
+++ dedupe/walker.py
@@ -4,28 +4,27 @@ from dedupe import event
 
 class DedupeWalker:
 
-    def __init__(self, database_api):
-        self.dbapi = database_api
-
-
     def process_file(self, file_path):
         if os.path.isfile(file_path):
-            print(f" --> {file_path}")
+            print(f"WALKER: Processing file: {file_path}")
             event.execute_handlers("FILE-HASH", file_path)
         else:
-            print(f"!! Could not locate file {file_path}")
+            print(f"WALKER: ERROR: !! Could not locate file {file_path}")
 
 
     def process_dir(self, dir_path):
         if os.path.isdir(dir_path):
-            print(f": {dir_path}")
+            print(f"WALKER: Processing dir : {dir_path}")
             event.execute_handlers("DIR-HASH", dir_path)
         else:
-            print(f"!! Could not locate dir {dir_path}")
+            print(f"WALKER: ERROR: !! Could not locate dir {dir_path}")
 
 
     def walk_folder(self, path):
         print(f"Processing {path}")
+
+        self.process_dir(path)
+
         for parent_dir, child_dirs, child_files in os.walk(path):
             for file_name in child_files:
                 self.process_file(os.path.join(parent_dir, file_name))
